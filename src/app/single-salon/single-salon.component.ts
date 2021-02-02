459fce4bcfa43ffa4253b000f37f1572bce7348b
ftplz <ya.zazaza77@gmail.com>
2021-02-02 17:15:59 +0200

    added comments

--- src/app/single-salon/single-salon.component.ts
+++ src/app/single-salon/single-salon.component.ts
@@ -5,6 +5,7 @@ import {Service} from '../entity/Service';
 import {Salon} from '../entity/Salon';
 import {Title} from '@angular/platform-browser';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {Comment} from '../entity/Comment';
 
 @Component({
   selector: 'app-single-salon',
@@ -16,6 +17,8 @@ export class SingleSalonComponent implements OnInit {
   services: Service[];
   salon: Salon;
   name: string;
+  comments: Comment[];
+
   constructor(private activatedRoute: ActivatedRoute, private httpClient: HttpClient,
               private title: Title, private router: Router,
               private modalService: NgbModal) {
@@ -28,9 +31,10 @@ export class SingleSalonComponent implements OnInit {
       title.setTitle(this.salon.name);
     });
     this.httpClient.get<Service[]>('http://localhost:8080/services/salon/' + this.id).subscribe(value => this.services = value);
+    this.httpClient.get<Comment[]>('http://localhost:8080/comments/salon/' + this.id).subscribe(value => this.comments = value);
   }
 
-  toOrderList (id, name, price, duration, salon, image){
+  toOrderList(id, name, price, duration, salon, image){
     if (localStorage.getItem('token')){
       const services = {id : id, name : name, price : price, duration : duration, salon : salon, image : image};
       sessionStorage.setItem('service' + id, JSON.stringify(services));
@@ -41,6 +45,15 @@ export class SingleSalonComponent implements OnInit {
 
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  }
+
+  sendComment(name, content){
+    const comment = {
+      name: name,
+      content: content,
+      salon: this.id
+    };
+    this.httpClient.post('http://localhost:8080/comments', comment).subscribe(value => console.log(value));
   }
 
   addService(name, price, duration){
